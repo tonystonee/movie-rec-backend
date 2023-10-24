4375d4bdeba7850a7f9722e4244e0eb5cb7b5352
Anthony Dionise <anthony.dionise@gmail.com>
2023-10-24 11:38:46 -0400

    Integrate OpenAI API

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,24 @@ import express, { Request, Response } from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript Express!');
+import OpenAI from 'openai';
+
+// apiKey param: defaults to 'My API Key',
+const openai = new OpenAI();
+
+async function main() {
+  const params: OpenAI.Chat.ChatCompletionCreateParams = {
+    messages: [{ role: 'user', content: 'Say this is a test' }],
+    model: 'gpt-3.5-turbo',
+  };
+  const chatCompletion: OpenAI.Chat.ChatCompletion = await openai.chat.completions.create(params);
+  
+  return chatCompletion;
+}
+
+app.get('/', async (req: Request, res: Response) => {
+  const resposne = await main();
+  res.send(resposne);
 });
 
 app.listen(port, () => {
